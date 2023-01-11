da0ce36067146342700aa984a1c9e996653ec0ff
Joshua <joshuagpatten@gmail.com>
2023-01-11 17:47:44 -0500

    intial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import printMe from './print.js';
+
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  element.innerHTML = "Test Text";
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+ element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
