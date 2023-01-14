66a7552e74940db1281188fa115697a31e029835
Joshua <joshuagpatten@gmail.com>
2023-01-14 13:45:46 -0500

    Add comments, Add delete button to each todo, Change priority to radio btns

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 import { clearContent, displayToDo, displayForm, removeForm, removeButtons, displayButtons } from './DOMStuff';
 import { todoFactory } from './todo.js';
 
-(function component() {
+//Add functionality to buttons
+(function intitialize() {
   
+  //Add event listener to erase buttton
   const eraseBTN = document.getElementById('eraseBTN');
   eraseBTN.addEventListener('click', function(){clearContent()});
 
+  //Add event listener to add todo button
   const addToDoBTN = document.getElementById('addToDoBTN');
   addToDoBTN.addEventListener('click', function(){
     removeButtons();
     displayForm();
   });
 
+  //Add event to submit form button
   const formBTN = document.getElementById('formBTN');
   formBTN.addEventListener('click', (event) => {
     const myForm = document.getElementById('myform');
-    const testToDo2 = todoFactory(title.value, description.value, dueDate.value, priority.value);
+    const testToDo2 = todoFactory(title.value, description.value, dueDate.value, document.querySelector('input[name="priority"]:checked').value);
     displayToDo(testToDo2);
     myForm.reset();
     removeForm();
     displayButtons();
-  })
-
- 
+  }) 
 })();
